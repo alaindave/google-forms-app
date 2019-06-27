2d0ed2361cca40df025d2b4609426aef50a2c486
Alain Davis <davisbedetse@gmail.com>
2019-06-27 02:37:21 -0400

    step 2 updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Title from "./components/Title-description";
 import Questions from "./components/Questions";
 
 const App = props => {
   const [questionArray, setQuestionArray] = useState([
-    { title: "", description: "" }
+    { title: " ", description: " " }
   ]);
   // const [buttonClicked, setButtonClicked] = useState(false);
-  //  const [showButton, setShowButton] = useState(true);
+  const [showButton, setShowButton] = useState(true);
   // const [newQuestion, setNewQuestion] = useState(0);
 
   // const handleClick = useCallback(() => {
@@ -22,17 +22,39 @@ const App = props => {
   //console.log("total questions", questionArray.length);
   //});
 
+  const handleChange = e => {
+    if (["title", "description"].includes(e.target.className)) {
+      let newQuestions = [...questionArray];
+      newQuestions[e.target.dataset.id][
+        e.target.className
+      ] = e.target.value.toUpperCase();
+      setQuestionArray(newQuestions);
+      console.log("questions array", questionArray);
+    }
+  };
+
   const addQuestion = () => {
     setQuestionArray([...questionArray, { title: "", description: "" }]);
+    setShowButton(false);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
   };
 
   return (
     <div className="App">
       <Title />
-      <button className="add-question" onClick={addQuestion}>
-        Add question1
-      </button>
-      <Questions questions={questionArray} />
+      {showButton && (
+        <button className="add-question" onClick={addQuestion}>
+          Add question1
+        </button>
+      )}
+      <Questions
+        questions={questionArray}
+        handleClick={addQuestion}
+        changeHandler={handleChange}
+      />
     </div>
   );
 };
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 
 const Questions = props => {
-  const [question, setQuestion] = useState(" ");
-  const [description, setDescription] = useState(" ");
+  //const [question, setQuestion] = useState(" ");
+  // const [title, setTitle] = useState(" ");
+
+  //const [description, setDescription] = useState(" ");
+  const [buttonStyle, setButtonStyle] = useState({
+    position: "relative",
+    top: 165,
+    fontSize: 15
+  });
 
   //const handleChange = e => {
   //  const { name, value } = e.target;
@@ -34,31 +41,49 @@ const Questions = props => {
     </React.Fragment>
 
   **/
+  const buttonClicked = () => {
+    // setButtonStyle({ display: "none" });
+    props.handleClick();
+  };
 
-  return props.questions.map((val, idx) => {
-    let questionId = `question-${idx}`,
-      titleId = `title-${idx}`;
+  return props.questions.map((val, id) => {
+    let titleId = `question-${id}`,
+      descriptionId = `description-${id}`;
     return (
-      <div key={idx} className="d-flex flex-column question-field">
-        <input
-          type="text"
-          name={questionId}
-          data-id={idx}
-          id={questionId}
-          className="title"
-          value={props.questions[idx].description}
-          placeholder="Question"
-        />
+      <div className="d-flex flex-row">
+        <div key={id} className="d-flex flex-column question-field">
+          <form>
+            <input
+              type="text"
+              name="title"
+              data-id={id}
+              id={titleId}
+              className="title"
+              placeholder="Question"
+              value={props.questions[id].title}
+              onChange={props.changeHandler}
+            />
 
-        <input
-          type="text"
-          name={questionId}
-          data-id={idx}
-          id={questionId}
-          className="textArea"
-          value={props.questions[idx].description}
-          placeholder="Question Description(optional)"
-        />
+            <textarea
+              name="description"
+              data-id={id}
+              id={descriptionId}
+              className="description"
+              placeholder="Question Description(optional)"
+              value={props.questions[id].description}
+              onChange={props.changeHandler}
+            />
+          </form>
+
+          <button style={buttonStyle} onClick={buttonClicked}>
+            Add question2
+          </button>
+        </div>
+
+        <div className="form-description">
+          <h1>{props.questions[id].title}</h1>
+          <p>{props.questions[id].description}</p>
+        </div>
       </div>
     );
   });
