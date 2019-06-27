5e7a79e629fdeb1f37fecec0b1679e41bc31260d
Alain Davis <davisbedetse@gmail.com>
2019-06-26 21:31:29 -0400

    step 2 completed

--- src/App.js
+++ src/App.js
@@ -4,32 +4,35 @@ import Title from "./components/Title-description";
 import Questions from "./components/Questions";
 
 const App = props => {
-  const [questionArray, setQuestionArray] = useState([]);
-  const [buttonClicked, setButtonClicked] = useState(false);
-  const [showButton, setShowButton] = useState(true);
-  const [newQuestion, setNewQuestion] = useState(0);
+  const [questionArray, setQuestionArray] = useState([
+    { title: "", description: "" }
+  ]);
+  // const [buttonClicked, setButtonClicked] = useState(false);
+  //  const [showButton, setShowButton] = useState(true);
+  // const [newQuestion, setNewQuestion] = useState(0);
 
-  const handleClick = useCallback(() => {
-    setButtonClicked(true);
-    // setShowButton(false);
-    // setNewQuestion(newQuestion + 1);
-    // console.log("no of questions", newQuestion);
-    //let new_array = questionArray.concat(Math.floor(Math.random() * 100 + 1));
-    setQuestionArray([...questionArray, <Questions />]);
-    console.log("question array", questionArray);
-    console.log("total questions", questionArray.length);
-  });
+  // const handleClick = useCallback(() => {
+  // setButtonClicked(true);
+  // setShowButton(false);
+  // setNewQuestion(newQuestion + 1);
+  // console.log("no of questions", newQuestion);
+  //let new_array = questionArray.concat(Math.floor(Math.random() * 100 + 1));
+  //setQuestionArray([...questionArray, <Questions />]);
+  // console.log("question array", questionArray);
+  //console.log("total questions", questionArray.length);
+  //});
+
+  const addQuestion = () => {
+    setQuestionArray([...questionArray, { title: "", description: "" }]);
+  };
 
   return (
     <div className="App">
       <Title />
-      {showButton && (
-        <button className="add-question" onClick={handleClick}>
-          Add question1
-        </button>
-      )}
-
-      {buttonClicked && questionArray}
+      <button className="add-question" onClick={addQuestion}>
+        Add question1
+      </button>
+      <Questions questions={questionArray} />
     </div>
   );
 };
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,16 +4,16 @@ const Questions = props => {
   const [question, setQuestion] = useState(" ");
   const [description, setDescription] = useState(" ");
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    if (name === "title") {
-      setQuestion(value);
-    } else {
-      setDescription(value);
-    }
-  };
+  //const handleChange = e => {
+  //  const { name, value } = e.target;
+  //if (name === "title") {
+  //  setQuestion(value);
+  // } else {
+  //  setDescription(value);
+  // }
+  //};
 
-  return (
+  /** return (
     <React.Fragment>
       <form className="d-flex flex-column">
         <input
@@ -32,7 +32,36 @@ const Questions = props => {
       <p>im component two</p>
       <p>component 2</p>
     </React.Fragment>
-  );
+
+  **/
+
+  return props.questions.map((val, idx) => {
+    let questionId = `question-${idx}`,
+      titleId = `title-${idx}`;
+    return (
+      <div key={idx} className="d-flex flex-column question-field">
+        <input
+          type="text"
+          name={questionId}
+          data-id={idx}
+          id={questionId}
+          className="title"
+          value={props.questions[idx].description}
+          placeholder="Question"
+        />
+
+        <input
+          type="text"
+          name={questionId}
+          data-id={idx}
+          id={questionId}
+          className="textArea"
+          value={props.questions[idx].description}
+          placeholder="Question Description(optional)"
+        />
+      </div>
+    );
+  });
 };
 
 export default Questions;
